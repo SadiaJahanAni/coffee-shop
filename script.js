7eb144f5a6c65872ca1aed979a94b4d6952fab13
SadiaJahanAni <sadiajahanani@gmail.com>
2023-11-07 01:23:59 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// for navbar.....
+let navbar=document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick=()=>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active')
+    cartItem.classList.remove('active')
+}
+
+// for search.....
+let searchForm=document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick=()=>{
+  searchForm.classList.toggle('active');
+  navbar.classList.remove('active')
+  cartItem.classList.remove('active')
+} 
+
+
+// for cartItem......
+let cartItem=document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick=()=>{
+  cartItem.classList.toggle('active');
+  searchForm.classList.remove('active');
+  navbar.classList.remove('active')
+} 
+
+window.onscroll=()=>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active')
+    cartItem.classList.remove('active')
+}
